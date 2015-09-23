09fd95bb05bbf45bc0db9b126f04be68c6c5b405
jaxgeller <jacksongeller@gmail.com>
2015-09-23 15:38:14 -0400

    counter added in

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,98 @@
-const shirts = document.querySelectorAll('.shirt-item');
-const overlay = document.getElementById('overlay');
-const title = document.getElementById('overlay-title');
-const sizes = document.getElementById('overlay-sizes');
+const d = document;
+
+const shirts = d.querySelectorAll('.shirt-item');
+const overlay = d.getElementById('overlay');
+const title = d.getElementById('overlay-title');
+const sizes = d.getElementById('overlay-sizes');
 
 [].slice.call(shirts).forEach((shirt) => {
   shirt.addEventListener('click', shirtClickHandler.bind(shirt));
 });
 
-function shirtClickHandler() {
-  overlay.style.backgroundColor = this.dataset.color;
-  title.textContent = this.alt;
-  overlay.classList.add('is-active');
-
-  this.style.transformOrigin = this.dataset.direction;
-  this.classList.add('is-active');
+let opts = {
+  selector: null,
+  end: null,
+  start: 0,
+  duration: 2000
 }
 
+function shirtClickHandler() {
+  let self = this;
 
-document.getElementById('overlay-back').addEventListener('click', function() {
-  [].slice.call(document.querySelectorAll('.is-active')).forEach(function(item) {
+  title.textContent = self.alt;
+  sizes.appendChild(setSizes(self.dataset.sizes));
+
+  [].slice.call(sizes.querySelectorAll('span')).forEach((item) => {
+    opts.selector = item;
+    opts.end = parseInt(item.textContent);
+
+    let a = new Counter(opts);
+    setTimeout(()=> { a.run(); }, 700)
+  });
+
+
+  overlay.style.backgroundColor = self.dataset.color;
+  overlay.classList.add('is-active');
+
+  self.style.transformOrigin = self.dataset.direction;
+  self.classList.add('is-active');
+}
+
+function setSizes(s) {
+  const holder = d.createDocumentFragment();
+
+  s.split(' ').forEach((item) => {
+    item = item.split('');
+
+    let span = d.createElement('span');
+    span.textContent = item[0];
+
+    let text = d.createTextNode(item[1]);
+
+    holder.appendChild(span);
+    holder.appendChild(text);
+  });
+
+  return holder;
+}
+
+d.getElementById('overlay-back').addEventListener('click', function() {
+  [].slice.call(d.querySelectorAll('.is-active')).forEach(function(item) {
     item.classList.remove('is-active');
+    sizes.innerHTML = '';
   });
 });
+
+
+
+class Counter {
+
+  constructor(opts) {
+    this.start = opts.start;
+    this.end = opts.end;
+    this.duration = opts.duration;
+    this.selector = opts.selector;
+  }
+
+  tick(currentTime) {
+    if(!this.timeStart) this.timeStart = currentTime;
+    this.timeElapsed = currentTime - this.timeStart;
+
+    this.next = this.ease(this.timeElapsed, this.start, this.end - this.start, this.duration);
+    this.next = Math.round(this.next);
+
+    this.selector.textContent = this.next;
+
+    if (this.next < this.end)
+      requestAnimationFrame((time)=> this.tick(time))
+  }
+
+  run() {
+    requestAnimationFrame(this.tick.bind(this));
+  }
+
+  // t: current time, b: begInnIng value, c: change In value, d: duration
+  ease(t, b, c, d) {
+    return c * (-Math.pow(2, -10 * t / d) + 1) * 1024 / 1023 + b;
+  }
+}
