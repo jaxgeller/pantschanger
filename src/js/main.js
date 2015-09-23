2431dbe77a972c8c3fc672a5b2c7ecd7774709d0
jaxgeller <jacksongeller@gmail.com>
2015-09-23 13:41:58 -0400

    minor animation improvements

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,10 @@ const title = document.getElementById('overlay-title');
 const sizes = document.getElementById('overlay-sizes');
 
 [].slice.call(shirts).forEach((shirt) => {
-  shirt.addEventListener('click', shirtClickHandler);
+  shirt.addEventListener('click', shirtClickHandler.bind(shirt));
 });
 
 function shirtClickHandler() {
-  var h = document.body.clientHeight;
-  overlay.style.height = (h + 'px');
   overlay.style.backgroundColor = this.dataset.color;
   title.textContent = this.alt;
   overlay.classList.add('is-active');
