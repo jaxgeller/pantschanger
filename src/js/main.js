fc44d64bfe39b58e5c119454eff98034baa4cf77
jaxgeller <jacksongeller@gmail.com>
2015-10-04 15:46:14 -0400

    add escape back button

--- src/js/main.js
+++ src/js/main.js
@@ -1,83 +1,86 @@
 import Counter from './counter.js';
 import Shirts from './shirts.js';
 
-const d = document;
-
-const shirts = d.querySelectorAll('.shirt-item');
-const overlay = d.getElementById('overlay');
-const title = d.getElementById('overlay-title');
-const sizes = d.getElementById('overlay-sizes');
-
-[].slice.call(shirts).forEach((shirt) => {
-  shirt.addEventListener('click', shirtClickHandler.bind(shirt));
-});
-
-let opts = {
-  selector: null,
-  end: null,
-  start: 0,
-  duration: 2000
-}
-
-function shirtClickHandler() {
-  let self = this;
-
-  if (!self.classList.contains('is-active')) {
-
-  title.textContent = self.alt;
-  sizes.appendChild(setSizes(self.dataset.sizes));
-
-  [].slice.call(sizes.querySelectorAll('span')).forEach((item) => {
-    opts.selector = item;
-    opts.end = parseInt(item.textContent);
-
-    let a = new Counter(opts);
-    setTimeout(()=> { a.run(); }, 700)
-  });
+const s = new Shirts();
 
 
-  overlay.style.backgroundColor = self.dataset.color;
-  overlay.classList.add('is-active');
+// const d = document;
 
-  let origin = [];
-  origin[0] = (-1 * (((window.innerWidth - 600)/2) - self.getBoundingClientRect().left + 10)) + 'px';
+// const shirts = document.querySelectorAll('.shirt-item');
+// const overlay = document.getElementById('overlay');
+// const title = d.getElementById('overlay-title');
+// const sizes = d.getElementById('overlay-sizes');
 
-  if (window.innerWidth < 960) {
-    document.body.classList.add('is-active');
-    origin[1] = ((self.getBoundingClientRect().top - 225) * 4) + 'px';
-    document.ontouchmove = function(e) {e.preventDefault()}
-  } else {
-    origin[1] = (self.getBoundingClientRect().top - 225) + 'px';
-  }
+// [].slice.call(shirts).forEach((shirt) => {
+//   shirt.addEventListener('click', shirtClickHandler.bind(shirt));
+// });
 
-  self.style.transformOrigin = origin.join(' ');
-  self.style.webkitTransformOrigin = origin.join(' ');
-  self.classList.add('is-active');
-  }
-}
+// let opts = {
+//   selector: null,
+//   end: null,
+//   start: 0,
+//   duration: 2000
+// }
 
-function setSizes(s) {
-  const holder = d.createDocumentFragment();
+// function shirtClickHandler() {
+//   let self = this;
 
-  s.split(' ').forEach((item) => {
-    item = item.split('');
+//   if (!self.classList.contains('is-active')) {
 
-    let span = d.createElement('span');
-    span.textContent = item[0];
+//   title.textContent = self.alt;
+//   sizes.appendChild(setSizes(self.dataset.sizes));
 
-    let text = d.createTextNode(item[1]);
+//   [].slice.call(sizes.querySelectorAll('span')).forEach((item) => {
+//     opts.selector = item;
+//     opts.end = parseInt(item.textContent);
 
-    holder.appendChild(span);
-    holder.appendChild(text);
-  });
+//     let a = new Counter(opts);
+//     setTimeout(()=> { a.run(); }, 700)
+//   });
 
-  return holder;
-}
 
-d.getElementById('overlay-back').addEventListener('click', function() {
-  [].slice.call(d.querySelectorAll('.is-active')).forEach(function(item) {
-    item.classList.remove('is-active');
-    sizes.innerHTML = '';
-    document.ontouchmove = function(e) {return true;}
-  });
-});
+//   overlay.style.backgroundColor = self.dataset.color;
+//   overlay.classList.add('is-active');
+
+//   let origin = [];
+//   origin[0] = (-1 * (((window.innerWidth - 600)/2) - self.getBoundingClientRect().left + 10)) + 'px';
+
+//   if (window.innerWidth < 960) {
+//     document.body.classList.add('is-active');
+//     origin[1] = ((self.getBoundingClientRect().top - 225) * 4) + 'px';
+//     document.ontouchmove = function(e) {e.preventDefault()}
+//   } else {
+//     origin[1] = (self.getBoundingClientRect().top - 225) + 'px';
+//   }
+
+//   self.style.transformOrigin = origin.join(' ');
+//   self.style.webkitTransformOrigin = origin.join(' ');
+//   self.classList.add('is-active');
+//   }
+// }
+
+// function setSizes(s) {
+//   const holder = d.createDocumentFragment();
+
+//   s.split(' ').forEach((item) => {
+//     item = item.split('');
+
+//     let span = d.createElement('span');
+//     span.textContent = item[0];
+
+//     let text = d.createTextNode(item[1]);
+
+//     holder.appendChild(span);
+//     holder.appendChild(text);
+//   });
+
+//   return holder;
+// }
+
+// d.getElementById('overlay-back').addEventListener('click', function() {
+//   [].slice.call(d.querySelectorAll('.is-active')).forEach(function(item) {
+//     item.classList.remove('is-active');
+//     sizes.innerHTML = '';
+//     document.ontouchmove = function(e) {return true;}
+//   });
+// });
--- src/js/shirts.js
+++ src/js/shirts.js
@@ -1,5 +1,9 @@
 import Counter from './counter';
 
+function iter(selector) {
+  return Array.prototype.slice.call(selector);
+}
+
 export default class Shirts {
   constructor() {
     this.overlay  = document.getElementById('overlay');
@@ -7,35 +11,88 @@ export default class Shirts {
     this.sizes    = document.getElementById('overlay-sizes');
     this.shirts   = document.querySelectorAll('.shirt-item');
 
-    this._addShirtClickHandler();
+    // Set Shirt Click Handler
+    iter(this.shirts).forEach(shirt => {
+      shirt.addEventListener('click', this.shirtClickHandler.bind(this), false);
+    });
+
+    // Set Back Click Handler
+    document.getElementById('overlay-back').addEventListener('click', this._reset.bind(this));
+
+    // Set Escape Key Handler
+    document.onkeyup = function(e){
+      let key = e.keyCode || e.which;
+      if (key === 27)
+        this._reset();
+    }.bind(this)
   }
 
-  // Click Handlers
-  _addShirtClickHandler() {
-    Array.prototype.slice.call(this.shirts).forEach(shirt => {
-      shirt.addEventListener('click', this._shirtClickHandler.bind(shirt));
+  shirtClickHandler(e) {
+    let el = e.target;
+    this._setOverlay(el);
+    this._setOrigin(el);
+
+    el.classList.add('is-active');
+  }
+
+  _setOrigin(el) {
+    let width = window.innerWidth;
+    let rect = el.getBoundingClientRect();
+
+    let origin = [];
+    origin[0] = (-1 * (((width - 600)/2) - rect.left + 10)) + 'px';
+
+    if (width < 960) {
+      origin[1] = ((rect.top - 225) * 4) + 'px';
+      document.ontouchmove = function(e) {e.preventDefault()}
+    } else {
+      origin[1] = (rect.top - 225) + 'px';
+    }
+
+    origin = origin.join(' ')
+
+    el.style.transformOrigin = origin;
+    el.style.webkitTransformOrigin = origin;
+  }
+  // Set overlay options
+  _setOverlay(el) {
+    this.title.textContent = el.alt;
+    this.sizes.appendChild(this._setSizes(el.dataset.sizes));
+    this.overlay.style.backgroundColor = el.dataset.color;
+    this.overlay.classList.add('is-active');
+    document.body.classList.add('is-active');
+
+    iter(this.sizes.querySelectorAll('span')).forEach(item => {
+      let c = new Counter({ selector: item, end: parseInt(item.textContent) });
+      setTimeout(() => { c.run(); }, 700)
     });
   }
-  _shirtClickHandler() {
-    console.log(this)
-  }
-
-  // Set overlay sizes
   _setSizes(s) {
-    const holder = d.createDocumentFragment();
+    const holder = document.createDocumentFragment();
 
     s.split(' ').forEach((item) => {
       item = item.split('');
 
-      let span = d.createElement('span');
+      let span = document.createElement('span');
       span.textContent = item[0];
 
-      let text = d.createTextNode(item[1]);
+      let text = document.createTextNode(item[1]);
 
       holder.appendChild(span);
       holder.appendChild(text);
     });
 
     return holder;
+  }
+
+  _reset() {
+    let active = document.querySelectorAll('.is-active');
+    let sizes = this.sizes;
+
+    iter(active).forEach(item => {
+      item.classList.remove('is-active');
+      sizes.innerHTML = '';
+      document.ontouchmove = function(e) {return true;}
+    });
   }
 }
